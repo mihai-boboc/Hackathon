62e1ea05bf5ec6499bb126f59a2aec8f3b022b93
AndreiBotezan <73821849+AndreiBotezan@users.noreply.github.com>
2021-06-26 09:58:27 +0300

    Initiated test project

---
+++ TimisoaraInventoryTestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TimisoaraInventoryTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
