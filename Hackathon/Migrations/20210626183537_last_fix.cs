ededdf5258e122d7b2ca6e75f84cc3f78fe4ad7b
Mihai Boboc <mihai.boboc@ness.com>
2021-06-27 09:32:19 +0300

    - db fixes

---
+++ Hackathon/Migrations/20210626183537_last_fix.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hackathon.Migrations
+{
+    public partial class last_fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
